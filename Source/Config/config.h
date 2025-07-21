cec7ad7dbacac3d00790dc89fd208e53aa5ecef3
Max Henry <mhenry@eers.ca>
2025-07-21 12:30:25 -0400

    remove config.h

--- Source/Config/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/**
- * LucidKaraoke Configuration
- */
-
-// Cloud Processing Service Configuration
-// Uncomment and set your service URL to skip the startup prompt:
-// #define SERVICE_URL "http://localhost:8000"
